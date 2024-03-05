9715e7dcb6aa165091325bb0a6bed23d11cfea3e
PsGarr <pes.garr@gmail.com>
2024-03-05 15:42:14 +0100

    First Commit

---
+++ src/app/index/index.tsx
@@ -0,0 +1,11 @@
+import { View,Text } from "react-native";
+
+export default function Index(){
+    return(
+        <View>
+            <Text>
+                Testing 1,2,3...
+            </Text>
+        </View>
+    )
+}
